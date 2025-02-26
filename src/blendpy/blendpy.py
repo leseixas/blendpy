9b88a8d2e3c01d53ae7c7c3e325b0d3a37653c75
Leandro Seixas <leandro.fisica@gmail.com>
2025-02-26 20:49:38 -0300

    initial Blendpy constructor

--- src/blendpy/blendpy.py
+++ src/blendpy/blendpy.py
@@ -2,6 +2,19 @@
 # -*- coding: utf-8 -*-
 # file: blendpy.py
 
-class Blendpy():
-  def __init__(self):
-    pass
+import numpy as np
+from ase.io import read
+from ase import Atoms
+
+class Blendpy(Atoms):
+  def __init__(self, method='dsi', alloy_basis=[], calculator = None, supercell=[1,1,1], suballoy = False):
+    super().__init__()
+    self.method = method
+    self.alloy_basis: list = alloy_basis
+    self.calculator = calculator
+    self.supercell = supercell
+    self.suballoy = suballoy
+
+
+if __name__ == '__main__':
+  alloy = Blendpy(calculator = calc)

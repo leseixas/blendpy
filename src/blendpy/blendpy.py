5e5d0fcc441cd776938536921bfa2a67dca5ccf0
Leandro Seixas <leandro.fisica@gmail.com>
2025-02-27 00:32:04 -0300

    initial methods for Blendpy class

--- src/blendpy/blendpy.py
+++ src/blendpy/blendpy.py
@@ -17,19 +17,42 @@ class Blendpy(Atoms):
     self.optimizer = optimizer
     self.supercell = supercell
     self.suballoy = suballoy
-#    self.dsi = 
+    self.atoms_alloy_basis = None # self.get_alloy_basis()
+    self.atoms_supercells = None # self.get_supercells()
+
+  def get_alloy_basis(self):
+    atoms_alloy_basis = []
+    for alloy in self.alloy_basis: 
+      atoms = read(alloy)
+      atoms_alloy_basis.append(atoms)
+    return atoms_alloy_basis
+
+  def get_supercells(self):
+    atoms_supercells = []
+    for alloy in self.atoms_alloy_basis:
+      alloy_sc = alloy.repeat(self.supercell) 
+      atoms_supercells.append(alloy_sc)
+    return atoms_supercells
 
 
 if __name__ == '__main__':
-  from gpaw import GPAW, PW, FermiDirac, Davidson, Mixer
-  calc = GPAW(mode=PW(400),
-              xc='PBE',
-              kpts=(7,7,7),
-              occupations=FermiDirac(5),
-              eigensolver=Davidson(5),
-              spinpol=False,
-              mixer=Mixer(0.05, 5, 100))
+  import warnings
+  warnings.filterwarnings("ignore")
+#  from gpaw import GPAW, PW, FermiDirac, Davidson, Mixer
+#  calc_gpaw = GPAW(mode=PW(400),
+#                   xc='PBE',
+#                   kpts=(7,7,7),
+#                   occupations=FermiDirac(5),
+#                   eigensolver=Davidson(5),
+#                   spinpol=False,
+#                   mixer=Mixer(0.05, 5, 100))
 
-  alloy = Blendpy(alloy_basis=['Au_fcc.vasp', 'Pt_fcc.vasp'],
-                  calculator = calc,
+  from mace.calculators import mace_mp
+  calc_mace = mace_mp(model="small",
+                      dispersion=False,
+                      default_dtype="float32",
+                      device='cpu')
+
+  alloy = Blendpy(alloy_basis=['../../test/Au.vasp', '../../Pt.vasp'],
+                  calculator = calc_mace,
                   supercell=[3,3,3])

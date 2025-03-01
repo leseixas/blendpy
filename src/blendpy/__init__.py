e715747a77afb2545a17335f15844e21a56bc742
Leandro Seixas <leandro.fisica@gmail.com>
2025-03-01 15:44:26 -0300

    Correct bug in __init__

--- src/blendpy/__init__.py
+++ src/blendpy/__init__.py
@@ -2,8 +2,7 @@
 # file: __init__.py
 
 from .alloy import Alloy
-from .blendpy import DSIModel 
-from .blendpy import version
+from .dsi_model import DSIModel
 from .polymorph import Polymorph
 
 __version__ = '25.3.0'
